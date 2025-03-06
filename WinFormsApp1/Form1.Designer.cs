beee8bd7ba928bce06734a9f8e381eb4ef791b78
student <student@DESKTOP-5POI41D>
2025-03-06 11:03:48 +0100

    Add project files.

---
+++ WinFormsApp1/Form1.Designer.cs
@@ -0,0 +1,251 @@
+﻿namespace WinFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button_equal = new Button();
+            button_7 = new Button();
+            button_6 = new Button();
+            button_9 = new Button();
+            button_plus = new Button();
+            button_minus = new Button();
+            button_3 = new Button();
+            button_multi = new Button();
+            button_8 = new Button();
+            button_5 = new Button();
+            button_2 = new Button();
+            button_0 = new Button();
+            button_div = new Button();
+            button_1 = new Button();
+            button_4 = new Button();
+            textBox1 = new TextBox();
+            SuspendLayout();
+            // 
+            // button_equal
+            // 
+            button_equal.Location = new Point(453, 207);
+            button_equal.Name = "button_equal";
+            button_equal.Size = new Size(137, 50);
+            button_equal.TabIndex = 0;
+            button_equal.Text = "=";
+            button_equal.UseVisualStyleBackColor = true;
+            button_equal.Click += button1_Click;
+            // 
+            // button_7
+            // 
+            button_7.BackColor = SystemColors.InactiveCaption;
+            button_7.Location = new Point(182, 207);
+            button_7.Name = "button_7";
+            button_7.Size = new Size(75, 23);
+            button_7.TabIndex = 1;
+            button_7.Text = "button7";
+            button_7.UseVisualStyleBackColor = false;
+            button_7.Click += button2_Click;
+            // 
+            // button_6
+            // 
+            button_6.Location = new Point(363, 238);
+            button_6.Name = "button_6";
+            button_6.Size = new Size(75, 23);
+            button_6.TabIndex = 2;
+            button_6.Text = "6";
+            button_6.UseVisualStyleBackColor = true;
+            button_6.Click += button_6_Click;
+            // 
+            // button_9
+            // 
+            button_9.Location = new Point(363, 209);
+            button_9.Name = "button_9";
+            button_9.Size = new Size(75, 23);
+            button_9.TabIndex = 3;
+            button_9.Text = "9";
+            button_9.UseVisualStyleBackColor = true;
+            button_9.Click += button4_Click;
+            // 
+            // button_plus
+            // 
+            button_plus.Location = new Point(466, 267);
+            button_plus.Name = "button_plus";
+            button_plus.Size = new Size(75, 23);
+            button_plus.TabIndex = 4;
+            button_plus.Text = "+";
+            button_plus.UseVisualStyleBackColor = true;
+            button_plus.Click += button_plus_Click;
+            // 
+            // button_minus
+            // 
+            button_minus.Location = new Point(466, 299);
+            button_minus.Name = "button_minus";
+            button_minus.Size = new Size(75, 23);
+            button_minus.TabIndex = 5;
+            button_minus.Text = "-";
+            button_minus.UseVisualStyleBackColor = true;
+            button_minus.Click += button_minus_Click;
+            // 
+            // button_3
+            // 
+            button_3.Location = new Point(363, 267);
+            button_3.Name = "button_3";
+            button_3.Size = new Size(75, 23);
+            button_3.TabIndex = 6;
+            button_3.Text = "3";
+            button_3.UseVisualStyleBackColor = true;
+            button_3.Click += button_3_Click;
+            // 
+            // button_multi
+            // 
+            button_multi.Location = new Point(354, 299);
+            button_multi.Name = "button_multi";
+            button_multi.Size = new Size(75, 23);
+            button_multi.TabIndex = 7;
+            button_multi.Text = "*";
+            button_multi.UseVisualStyleBackColor = true;
+            button_multi.Click += button_multi_Click;
+            // 
+            // button_8
+            // 
+            button_8.Location = new Point(273, 209);
+            button_8.Name = "button_8";
+            button_8.Size = new Size(75, 23);
+            button_8.TabIndex = 8;
+            button_8.Text = "8";
+            button_8.UseVisualStyleBackColor = true;
+            button_8.Click += button_8_Click;
+            // 
+            // button_5
+            // 
+            button_5.Location = new Point(273, 238);
+            button_5.Name = "button_5";
+            button_5.Size = new Size(75, 23);
+            button_5.TabIndex = 9;
+            button_5.Text = "5";
+            button_5.UseVisualStyleBackColor = true;
+            // 
+            // button_2
+            // 
+            button_2.Location = new Point(273, 270);
+            button_2.Name = "button_2";
+            button_2.Size = new Size(75, 23);
+            button_2.TabIndex = 10;
+            button_2.Text = "2";
+            button_2.UseVisualStyleBackColor = true;
+            // 
+            // button_0
+            // 
+            button_0.Location = new Point(273, 299);
+            button_0.Name = "button_0";
+            button_0.Size = new Size(75, 23);
+            button_0.TabIndex = 11;
+            button_0.Text = "0";
+            button_0.UseVisualStyleBackColor = true;
+            // 
+            // button_div
+            // 
+            button_div.Location = new Point(182, 299);
+            button_div.Name = "button_div";
+            button_div.Size = new Size(75, 23);
+            button_div.TabIndex = 12;
+            button_div.Text = "/";
+            button_div.UseVisualStyleBackColor = true;
+            // 
+            // button_1
+            // 
+            button_1.Location = new Point(182, 270);
+            button_1.Name = "button_1";
+            button_1.Size = new Size(75, 23);
+            button_1.TabIndex = 13;
+            button_1.Text = "1";
+            button_1.UseVisualStyleBackColor = true;
+            button_1.Click += button_1_Click;
+            // 
+            // button_4
+            // 
+            button_4.Location = new Point(182, 238);
+            button_4.Name = "button_4";
+            button_4.Size = new Size(75, 23);
+            button_4.TabIndex = 14;
+            button_4.Text = "4";
+            button_4.UseVisualStyleBackColor = true;
+            button_4.Click += button15_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(182, 53);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(382, 23);
+            textBox1.TabIndex = 15;
+            textBox1.TextChanged += textBox1_TextChanged;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.InactiveCaptionText;
+            ClientSize = new Size(800, 450);
+            Controls.Add(textBox1);
+            Controls.Add(button_4);
+            Controls.Add(button_1);
+            Controls.Add(button_div);
+            Controls.Add(button_0);
+            Controls.Add(button_2);
+            Controls.Add(button_5);
+            Controls.Add(button_8);
+            Controls.Add(button_multi);
+            Controls.Add(button_3);
+            Controls.Add(button_minus);
+            Controls.Add(button_plus);
+            Controls.Add(button_9);
+            Controls.Add(button_6);
+            Controls.Add(button_7);
+            Controls.Add(button_equal);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button_equal;
+        private Button button_7;
+        private Button button_6;
+        private Button button_9;
+        private Button button_plus;
+        private Button button_minus;
+        private Button button_3;
+        private Button button_multi;
+        private Button button_8;
+        private Button button_5;
+        private Button button_2;
+        private Button button_0;
+        private Button button_div;
+        private Button button_1;
+        private Button button_4;
+        private TextBox textBox1;
+    }
+}
---
+++ WinFormsApp1/Form1.cs
@@ -0,0 +1,71 @@
+using kalulator;
+namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        calculator kalkul = new calculator();
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button15_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button_8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button_1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button_plus_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button_minus_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button_multi_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button_3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button_6_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
